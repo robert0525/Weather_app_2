21b1be35bcf4ae63739b54e7921f1c1ff2c4466e
Robert Rydlewski <robertrydlewski1984@gmail.com>
2020-04-06 21:49:14 +0200

    adding image to weather conditions

--- app.js
+++ app.js
@@ -19,7 +19,33 @@ function searchWeather(searchTerm) {
     })
 }
 function init(resultFromServer) {
-    console.log(resultFromServer);
+    switch (resultFromServer.weather[0].main){
+        case 'Clear':
+            document.body.style.backgroundImage = 'url("img/clear.jpg")';
+            break;
+
+        case 'Clouds':
+            document.body.style.backgroundImage = 'url("img/clear.jpg")';
+                break;
+            
+
+        case 'Rain':
+        case 'Drizzle' :
+        case 'Mist':
+            document.body.style.backgroundImage = 'url("img/rain.jpg")';
+            break;
+
+        case 'Thunderstorm':
+            document.body.style.backgroundImage = 'url("img/thunderstorm.jpg")';
+            break;
+        
+        case 'Snow':
+            document.body.style.backgroundImage = 'url("img/snow.jpg")';
+            break;
+
+        default:
+            break;
+    }
     
 }
 
